28f9ff1c778d24ac6f49c756c57acd58c8bb0375
Suryansh Anand <spirit@anandsur.com>
2024-09-01 00:22:07 +0530

    feat: basic authed header

---
+++ apps/web/src/app/(authenticated)/Header.tsx
@@ -0,0 +1,48 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@web/components/ui/avatar";
+import { Button } from "@web/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@web/components/ui/tooltip";
+import { getServerAuthSession } from "@web/server/auth";
+import { Home } from "lucide-react";
+import Link from "next/link";
+
+export default async function Header() {
+  const session = await getServerAuthSession();
+
+  return (
+    <header className="p-4">
+      <nav className="container mx-auto flex items-center justify-between">
+        <Link href="/dashboard">
+          <span className="sr-only">Peacocking</span>
+          <img alt="" src="/logo.png" className="h-12 w-auto" />
+        </Link>
+
+        <div>
+          <Link href="/dashboard">Dashboard</Link>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Avatar>
+                  <AvatarImage src={session?.user.image ?? ""} />
+                  <AvatarFallback>U</AvatarFallback>
+                </Avatar>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="text-xs">{session?.user.email}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <Button>Sign Out</Button>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
