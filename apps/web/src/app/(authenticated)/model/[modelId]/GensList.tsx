355265f0449c094d77be369909493d10e80b45b3
Suryansh Anand <spirit@anandsur.com>
2024-09-02 14:42:30 +0530

    feat: FAQ section

--- apps/web/src/app/(authenticated)/model/[modelId]/GensList.tsx
+++ apps/web/src/app/(authenticated)/model/[modelId]/GensList.tsx
@@ -25,7 +25,7 @@ export default function GensList({ modelId }: { modelId: string }) {
             <img
               src={image.url}
               alt="Peacocked"
-              className="h-72 w-auto rounded-lg border border-transparent object-cover hover:border-primary"
+              className="h-72 w-auto min-w-[216.5px] rounded-lg border border-transparent object-cover hover:border-primary"
             />
             <TooltipProvider>
               <Tooltip>
--- apps/web/src/app/(landing)/Hero.tsx
+++ apps/web/src/app/(landing)/Hero.tsx
@@ -84,8 +84,8 @@ export default function Hero() {
                 <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                   <div className="relative">
                     <img
-                      alt="swordman"
-                      src="/sword.webp"
+                      alt="man in suit"
+                      src="/man-in-suit.jpeg"
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
@@ -102,8 +102,8 @@ export default function Hero() {
                   </div>
                   <div className="relative">
                     <img
-                      alt="gym boy"
-                      src="/gym.jpeg"
+                      alt="man water"
+                      src="/man-water.jpeg"
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                       style={{
                         objectPosition: "73% 50%",
@@ -115,22 +115,22 @@ export default function Hero() {
                 <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                   <div className="relative">
                     <img
-                      alt="og"
-                      src="/og.jpeg"
+                      alt="man street art"
+                      src="/man-street-art.jpeg"
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                       style={{
-                        objectPosition: "68% 50%",
+                        objectPosition: "200% 50%",
                       }}
                     />
                     <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                   </div>
                   <div className="relative">
                     <img
-                      alt="girl"
-                      src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                      alt="hiker"
+                      src="/hike-man.jpeg"
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                       style={{
-                        objectPosition: "73% 50%",
+                        objectPosition: "200% 50%",
                       }}
                     />
                     <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
