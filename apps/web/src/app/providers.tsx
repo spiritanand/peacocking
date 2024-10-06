c0dc3b06c3753d33e7478635bd338450e20eb5cd
Suryansh Anand <spirit@anandsur.com>
2024-10-06 14:34:05 +0530

    chore: disable PH autocapture

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -13,7 +13,8 @@ if (typeof window !== "undefined") {
     ui_host: "https://us.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-    capture_pageleave: true, // Enable pageleave capture
+    capture_pageleave: true, // Enable pageleave capture,
+    autocapture: false,
   });
 }
 
